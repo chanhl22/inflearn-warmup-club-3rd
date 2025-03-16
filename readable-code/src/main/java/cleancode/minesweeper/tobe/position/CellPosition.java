caae5ada14ac835335c1230e322045ef21d4c70a
chanhl22 <lch9502@naver.com>
2025-03-16 17:52:36 +0900

    refactor: Value Object - CellPosition 적용

--- readable-code/src/main/java/cleancode/minesweeper/tobe/position/CellPosition.java
+++ readable-code/src/main/java/cleancode/minesweeper/tobe/position/CellPosition.java
@@ -8,7 +8,7 @@ public class CellPosition {
     private final int colIndex;
 
     private CellPosition(int rowIndex, int colIndex) {
-        if(rowIndex < 0 || colIndex < 0) {
+        if (rowIndex < 0 || colIndex < 0) {
             throw new IllegalArgumentException("올바르지 않은 좌표입니다.");
         }
         this.rowIndex = rowIndex;
@@ -30,6 +30,45 @@ public class CellPosition {
     @Override
     public int hashCode() {
         return Objects.hash(rowIndex, colIndex);
+    }
+
+    public boolean isRowIndexMoreThanOrEqual(int rowIndex) {
+        return this.rowIndex >= rowIndex;
+    }
+
+    public boolean isColIndexMoreThanOrEqual(int colIndex) {
+        return this.colIndex >= colIndex;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public int getColIndex() {
+        return colIndex;
+    }
+
+    public CellPosition calculatePositionBy(RelativePosition relationPosition) {
+        if (this.canCalculatePositionBy(relationPosition)) {
+            return CellPosition.of(
+                    this.rowIndex + relationPosition.getDeltaRow(),
+                    this.colIndex + relationPosition.getDeltaCol()
+            );
+        }
+        throw new IllegalArgumentException("움직일 수 있는 좌표가 아닙니다.");
+    }
+
+    public boolean canCalculatePositionBy(RelativePosition relationPosition) {
+        return rowIndex + relationPosition.getDeltaRow() >= 0
+                && colIndex + relationPosition.getDeltaCol() >= 0;
+    }
+
+    public boolean isRowIndexLessThan(int rowSize) {
+        return this.rowIndex < rowSize;
+    }
+
+    public boolean isColIndexLessThan(int colSize) {
+        return this.colIndex < colSize;
     }
 
 }
