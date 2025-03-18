e6272f89c1993084d585a8f3d186ec582c6a8f37
chanhl22 <lch9502@naver.com>
2025-03-18 13:31:51 +0900

    refactor: Enum을 활용한 다형성

--- readable-code/src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ readable-code/src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -4,6 +4,7 @@ import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
 import cleancode.minesweeper.tobe.cell.CellSnapshot;
 import cleancode.minesweeper.tobe.io.sign.CellSignFinder;
+import cleancode.minesweeper.tobe.io.sign.CellSignProvider;
 import cleancode.minesweeper.tobe.position.CellPosition;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.stream.IntStream;
 
 public class ConsoleOutputHandler implements OutputHandler {
 
-    private final CellSignFinder cellSignFinder = new CellSignFinder();
+//    private final CellSignFinder cellSignFinder = new CellSignFinder();
 
     @Override
     public void showGameStartComments() {
@@ -32,7 +33,8 @@ public class ConsoleOutputHandler implements OutputHandler {
                 CellPosition cellPosition = CellPosition.of(row, col);
 
                 CellSnapshot snapshot = board.getSnapshot(cellPosition);
-                String cellSign = cellSignFinder.findCellSignFrom(snapshot);
+//                String cellSign = cellSignFinder.findCellSignFrom(snapshot);
+                String cellSign = CellSignProvider.findCellSignFrom(snapshot);
 
                 System.out.print(cellSign + " ");
             }
